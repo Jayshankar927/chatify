7998b78dd67b14e19cc653fd6548177133ea642a
Jayashankara M <jayashankara2001@gmail.com>
2026-08-10 07:12:59 +0000

    chore: initialize project layout with React, Tailwind v4, Node.js backend, and devops manifests

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+function App(){
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 class = "text-2xl font-medium">Hello World !!!</h1>
+    </div>
+  )
+}
+
+export default App;
